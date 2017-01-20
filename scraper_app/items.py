23b6264a02baf17575dc013270367db32752ad0b
Patrick Nicholls <padawagon@gmail.com>
2017-01-20 21:13:39 +1300

    Add all the things specified in the tutorial

--- scraper_app/items.py
+++ scraper_app/items.py
@@ -2,4 +2,9 @@ from scrapy.item import Item, Field
 
 class LivingSocialDeal(Item):
     '''Livingsocial container for scraped data'''
-
+    title = Field()
+    link = Field()
+    location = Field()
+    original_price = Field()
+    price = Field()
+    end_date = Field()
