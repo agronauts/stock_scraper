6cd7a270a38ee9c3f6cb102f9ea3f2ad3a80feee
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-24 11:37:06 +1300

    Persistent CSV storage

--- scraper_app/google_stock.py
+++ scraper_app/google_stock.py
@@ -2,14 +2,22 @@ import bs4
 import requests
 import pprint
 import re
+import csv
+from datetime import date
 
 PRICE_RE = re.compile('^\d+\.\d+$')
 codes = ['ATM','AFC','AFT','A01C','WIN'] # TODO get from search page
-for code in codes:
-    page = requests.get('https://www.google.com/finance?q=NZE%3A' + code)
-    soup = bs4.BeautifulSoup(page.content, 'html.parser')
-    # TODO verify numbers
-    stock_price = float(soup.find(name='span', text=PRICE_RE).text)
-    pprint.pprint(stock_price)
-    # TODO write results to file
-    # TODO log errors in seperate file
+with open('stock_prices.csv', 'a') as f:
+    csvwriter = csv.writer(f)
+    for code in codes:
+        page = requests.get('https://www.google.com/finance?q=NZE%3A' + code)
+        soup = bs4.BeautifulSoup(page.content, 'html.parser')
+        try:
+            stock_price = float(soup.find(name='span', text=PRICE_RE).text)
+        except ValueError:
+            print('Error in extracting stock price')
+        pprint.pprint(stock_price)
+        # TODO write results to file
+        csvwriter.writerow([date.today(), code, stock_price])
+
+        # TODO log errors in seperate file
