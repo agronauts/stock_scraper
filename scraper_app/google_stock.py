a04dd214d3af4ee05c92d89f4559304b1982b706
Patrick Nicholls <patrickn@estaroffice.local>
2017-01-24 11:20:46 +1300

    Stock script extracts singular stock price

--- scraper_app/google_stock.py
+++ scraper_app/google_stock.py
@@ -1,9 +1,15 @@
 import bs4
 import requests
 import pprint
-codes = ['ATM','AFC','AFT','A01C','WIN']
+import re
+
+PRICE_RE = re.compile('^\d+\.\d+$')
+codes = ['ATM','AFC','AFT','A01C','WIN'] # TODO get from search page
 for code in codes:
     page = requests.get('https://www.google.com/finance?q=NZE%3A' + code)
     soup = bs4.BeautifulSoup(page.content, 'html.parser')
-    soup.find_all('span')
-    pprint.pprint([tag.text for tag in soup.find_all('span') if 'id="ref' in str(tag) and 'l">' in str(tag)])
+    # TODO verify numbers
+    stock_price = float(soup.find(name='span', text=PRICE_RE).text)
+    pprint.pprint(stock_price)
+    # TODO write results to file
+    # TODO log errors in seperate file
