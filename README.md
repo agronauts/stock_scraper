fa2a9eb484177b0c8da2192914eed0cf6023e941
Patrick Nicholls <padawagon@gmail.com>
2017-03-28 13:53:29 +1300

    Finish ASX scraping script

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-TODO
+##TODO
+- NEED ASX prices
 - REFACTOR look at scrapy for getting trading codes
 - FEATURE add docopt
 - FEATURE email people
+    - Connect to free email service and send attachment
+    - https://www.mail.com/mail/attachments-up-to-50-MB/
+- FEATURE use numpy to rearrange data
 - REFACTOR use mongodb
+
+##Requirements
+- Closing price each day (16:00)
+- Stocks in NZX, ASX 
+– Stocks in alphabetical order in one file
+- Upload them into my database once a week or occasionally every two weeks if I’m busy
--- scraper_app/asx_stock.py
+++ scraper_app/asx_stock.py
@@ -1,3 +1,20 @@
 '''Get stock prices from ASX site (http://www.asx.com.au)'''
 
-QUERY_URL = "http://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes=BEN+MCY"
+QUERY_URL_FMT = "http://www.asx.com.au/asx/markets/equityPrices.do?by=asxCodes&asxCodes=%s"
+
+
+import bs4
+import requests as r
+
+def is_price_row(row):
+    return 'class' in row.attrs
+
+def get_stock_prices(code_prices):
+    page = r.get(QUERY_URL_FMT % '+'.join(codes)).content.decode()
+    soup = bs4.BeautifulSoup(page, 'html.parser')
+    rows = list(row for row in soup.find_all('tr') if is_price_row(row))
+    return {row.find('th').text.strip(): row.find('td').text.strip() for row in rows}
+
+
+codes = ['BEN', 'MCY', 'ACX', 'WHC']
+print(get_stock_prices(codes))
